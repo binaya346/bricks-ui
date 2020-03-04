382f3311c19113efabdcc05f3a5f43053f0cec9d
Binaya Rijal <rijalbinaya2@gmail.com>
2020-03-04 16:02:51 +0545

    adds buttons and tooltip pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { Button } from "./components/buttons/index";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Buttons from "./pages/Buttons";
+import Tooltip from "./pages/Tooltip";
 
 function App() {
   return (
     <div className="App">
-      <Button type="btn primary">Primary large</Button>
-      <Button type="btn danger btn-lg round">Danger large</Button>
-      <Button type="btn warning round">Warning round</Button>
-      <Button type="btn success round">Success round</Button>
-      <Button type="default btn btn-sm ">Default small</Button>
-      <Button type="primary btn btn-sm ">Primary small</Button>
-      <Button type="danger btn circular">Danger circular</Button>
-      <Button type="btn warning">Warning </Button>
-      <Button type="btn success">Success</Button>
-      <Button type="btn default">Default</Button>
-      <Button type="btn primary block">Primary</Button>
-      <Button type="btn warning block">Warning</Button>
-      <Button type="btn success block">Success</Button>
-      <Button type="btn danger block">Danger</Button>
+      <Router>
+        <Switch>
+          <Route path="/buttons">
+            <Buttons />
+          </Route>
+          <Route path="/tooltip">
+            <Tooltip />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -3,5 +3,32 @@ import "./index.scss";
 import { ButtonProps } from "../../Interfaces";
 
 export const Button = (props: ButtonProps) => {
-  return <button className={props.type}>{props.children}</button>;
+  if (props.href)
+    return (
+      <a
+        href={props.href}
+        className={`${props.type ? props.type : "default"} ${
+          props.shape ? props.shape : ""
+        } ${props.size ? props.size : ""} ${
+          props.className ? props.className : ""
+        }btn ${props.disabled ? " disabled" : ""} ${
+          props.block ? " block" : ""
+        }`}
+      >
+        {props.children}
+      </a>
+    );
+  return (
+    <button
+      type="button"
+      className={`${props.type ? props.type : "default"} ${
+        props.shape ? props.shape : ""
+      } ${props.size ? props.size : ""} ${
+        props.className ? props.className : ""
+      }btn ${props.disabled ? " disabled" : ""} ${props.block ? " block" : ""}`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
